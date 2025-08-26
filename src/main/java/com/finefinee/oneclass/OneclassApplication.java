df498147403fb9afa09b5ab928bc633f4371e417
김연호 <162892343+Finefinee@users.noreply.github.com>
2025-08-26 20:32:46 +0900

    Add files via upload

---
+++ src/main/java/com/finefinee/oneclass/OneclassApplication.java
@@ -0,0 +1,13 @@
+package com.finefinee.oneclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finefinee/oneclass/OneclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finefinee.oneclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
