656a4e99a40471e962adbdb44eb690df308c241a
김준현 <smileehd03@dgsw.hs.kr>
2025-08-28 00:10:04 +0900

    :JWE추가 &yml에서 JWT 비트수 증가

--- src/main/java/oneclass/oneclass/auth/jwt/JWTProvider.java
+++ src/main/java/oneclass/oneclass/auth/jwt/JWTProvider.java
@@ -1,7 +1,11 @@
 package oneclass.oneclass.auth.jwt;
 
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectDecrypter;
+import com.nimbusds.jose.crypto.DirectEncrypter;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import oneclass.oneclass.auth.dto.ResponseToken;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,7 +37,7 @@ public class JWTProvider implements InitializingBean {
     }
     @PostConstruct
     public void afterPropertiesSet() throws Exception {
-        this.key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
     //토큰생성
     public ResponseToken generateToken(String username){
@@ -56,6 +60,56 @@ public class JWTProvider implements InitializingBean {
                             .signWith(key, SignatureAlgorithm.HS256)
                             .compact();
         return new ResponseToken(accessToken, refreshToken);
+    }
+    //JWE 암호화
+    public String encyptToken(String PlainJwt) throws Exception{
+        byte[] aesKeyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        SecretKeySpec aesKey = new SecretKeySpec(aesKeyBytes, "AES");
+        JWEHeader header = new JWEHeader(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256);
+        Payload payload = new Payload(PlainJwt);
+        JWEObject jweObject = new JWEObject(header, payload);
+        jweObject.encrypt(new DirectEncrypter(aesKey.getEncoded()));
+        return jweObject.serialize();
+    }
+    //JWE 복호화
+    public String decyptToken(String jwtToken) throws Exception{
+        byte[] aesKeyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        SecretKeySpec aesKey = new SecretKeySpec(aesKeyBytes, "AES");
+        JWEObject jweObject = JWEObject.parse(jwtToken);
+        jweObject.decrypt(new DirectDecrypter(aesKey.getEncoded()));
+        return jweObject.getPayload().toString();
+    }
+    //토큰 생성(JWE 적용)
+    public ResponseToken generateTokenJwe(String username){
+        Date now = new Date();
+        //aceess token 생성
+        Date accessExpiry = new Date(now.getTime() + tokenValidityInMilliseconds);
+        String accessToken = Jwts.builder()
+                            .setSubject(username)
+                            .setIssuedAt(now)
+                            .setExpiration(accessExpiry)
+                            .signWith(key, SignatureAlgorithm.HS256)
+                            .compact();
+
+        //refresh token 생성
+        Date refreshExpiry = new Date(now.getTime() + tokenValidityInMilliseconds * 2);
+        String refreshToken = Jwts.builder()
+                            .setSubject(username)
+                            .setIssuedAt(now)
+                            .setExpiration(refreshExpiry)
+                            .signWith(key, SignatureAlgorithm.HS256)
+                            .compact();
+        //암호화 적용
+        String encryptedAccessToken;
+        String encryptedRefreshToken;
+
+        try{
+            encryptedAccessToken = encyptToken(accessToken);
+            encryptedRefreshToken = encyptToken(refreshToken);
+        }catch (Exception e){
+            throw new RuntimeException("토큰 암호화 실패",e);
+        }
+        return new ResponseToken(encryptedAccessToken, encryptedRefreshToken);
     }
 
     //토큰검증
