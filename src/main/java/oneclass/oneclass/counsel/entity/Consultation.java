5659c9c6331c946f3461f950e32670fcedbdfbc4
김준현 <smileehd03@dgsw.hs.kr>
2025-08-30 10:45:50 +0900

    :상담신청 추가

---
+++ src/main/java/oneclass/oneclass/counsel/entity/Consultation.java
@@ -0,0 +1,29 @@
+package oneclass.oneclass.counsel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Consultation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+
+    private String type;// 신규 or 재학생
+    private String subject;//과목
+    private String description;//상담 내용
+
+    private String status;//상담 신청됨(대기중) or 상담 날짜 확정
+    private LocalDateTime scheduleTime;
+
+    private LocalDateTime createAt;
+}
---
+++ src/main/java/oneclass/oneclass/counsel/repository/ConsultationRepository.java
@@ -0,0 +1,10 @@
+package oneclass.oneclass.counsel.repository;
+
+import oneclass.oneclass.counsel.entity.Consultation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+    List<Consultation> findByStatus(String status);
+}
