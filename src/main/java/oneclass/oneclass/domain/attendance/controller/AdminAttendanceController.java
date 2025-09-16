773483d925db2ef3b1b85a30db9372d87294c3e7
김연호 <kimfine000@gmail.com>
2025-09-16 20:33:25 +0900

    refactor: 엔드포인트 앞의 /api 모두 삭제

--- src/main/java/oneclass/oneclass/domain/attendance/controller/AdminAttendanceController.java
+++ src/main/java/oneclass/oneclass/domain/attendance/controller/AdminAttendanceController.java
@@ -13,7 +13,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin/attendance")
+@RequestMapping("/admin/attendance")
 @RequiredArgsConstructor
 public class AdminAttendanceController {
 
