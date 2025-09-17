c8cafe0d7c7086edd546f5557701dcc52269ade4
김연호 <kimfine000@gmail.com>
2025-09-17 23:38:18 +0900

    feat: 메세지 발송 진짜 완성

--- src/main/java/oneclass/oneclass/domain/message/sms/shortmessage/SmsCancelShortMessageScenario.java
+++ src/main/java/oneclass/oneclass/domain/message/sms/shortmessage/SmsCancelShortMessageScenario.java
@@ -13,17 +13,22 @@ import java.time.OffsetDateTime;
 import java.util.Arrays;
 
 @RequiredArgsConstructor
+@Deprecated
 public class SmsCancelShortMessageScenario extends BaseScenario implements ExecutableWithMessage {
 
   private final MemberRepository memberRepository;
 
   @Override
-  @Deprecated
   public void execute(String message) {
     OffsetDateTime reservationTime = OffsetDateTime.now().plusMinutes(30);
     Reservation reservation = new Reservation(reservationTime.toString());
-    SendSms sendSms = sendon.sms.sendSms(SMS_MOBILE_FROM, Arrays.asList(SMS_MOBILE_TO), message, true, reservation);
-    Log.d("SendSms: " + gson.toJson(sendSms));
+    SendSms sendSms = sendon.sms.sendSms(
+            SMS_MOBILE_FROM,
+            Arrays.asList(SMS_MOBILE_TO),
+            message,
+            true,
+            reservation);
+    Log.d("SendSms: " + gson.toJson(sendSms)); // sendon의 Log는 베포에 적합하지 않습니다.
 
     sleep(5000);
 
