c5346e250ea80eebac6b4f77c9c3af91a1258a4b
김연호 <kimfine000@gmail.com>
2025-10-01 17:00:48 +0900

    docs: AdminAttendanceController, AdminTaskController에 대한 Swagger 문서 추가

--- src/main/java/oneclass/oneclass/domain/task/controller/AdminTaskController.java
+++ src/main/java/oneclass/oneclass/domain/task/controller/AdminTaskController.java
@@ -1,5 +1,7 @@
 package oneclass.oneclass.domain.task.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import oneclass.oneclass.domain.task.dto.response.MemberTaskResponse;
 import oneclass.oneclass.domain.task.entity.TaskStatus;
@@ -16,9 +18,11 @@ public class AdminTaskController {
     private final AdminTaskService adminTaskService;
 
     @GetMapping("/{taskId}/members")
+    @Operation(summary = "과제별 회원 조회",
+            description = "특정 과제 ID에 대해 특정 상태(TaskStatus)에 해당하는 회원 리스트를 반환")
     public List<MemberTaskResponse> getMembersByTaskStatus(
-            @PathVariable Long taskId,
-            @RequestParam TaskStatus status) {
+            @Parameter(description = "조회할 과제 ID") @PathVariable Long taskId,
+            @Parameter(description = "조회할 과제 상태") @RequestParam TaskStatus status) {
         return adminTaskService.findMemberByTaskStatus(taskId, status);
     }
 
