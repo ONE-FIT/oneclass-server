0f71ee4271de89aadf053e44a29dc7cd3a2256d0
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-10-28 20:22:27 +0900

    Update UpdateLessonRequest.java

--- src/main/java/oneclass/oneclass/domain/lesson/dto/request/UpdateLessonRequest.java
+++ src/main/java/oneclass/oneclass/domain/lesson/dto/request/UpdateLessonRequest.java
@@ -1,11 +1,9 @@
 package oneclass.oneclass.domain.lesson.dto.request;
 
-import oneclass.oneclass.domain.member.entity.Member;
 
 public record UpdateLessonRequest(
         Long lessonId,
         String title,
-        Long teacherId,
-        Member studentId
+        Long teacherId
 ) {
 }
