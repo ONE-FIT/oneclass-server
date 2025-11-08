52b3fc1f4859a8cbeff37da774d4d2b3aa950385
김연호 <kimfine000@gmail.com>
2025-11-08 22:32:36 +0900

    refactor: lessonResponse가 정확한 데이터를 반환하도록 수정

--- src/main/java/oneclass/oneclass/domain/lesson/dto/response/LessonResponse.java
+++ src/main/java/oneclass/oneclass/domain/lesson/dto/response/LessonResponse.java
@@ -1,6 +1,6 @@
 package oneclass.oneclass.domain.lesson.dto.response;
 
-
+import java.util.List;
 
 import oneclass.oneclass.domain.lesson.entity.Lesson;
 import oneclass.oneclass.domain.member.entity.Member;
@@ -8,13 +8,27 @@ import oneclass.oneclass.domain.member.entity.Member;
 public record LessonResponse(
         Long lessonId,
         String title,
-        Member teacher
+        Long teacherId,
+        String teacherName,
+        List<Long> studentIds,
+        List<String> studentNames
 ) {
     public static LessonResponse of(Lesson lesson) {
+        List<Long> studentIds = lesson.getStudents().stream()
+                .map(Member::getId)
+                .toList();
+        
+        List<String> studentNames = lesson.getStudents().stream()
+                .map(Member::getName)
+                .toList();
+        
         return new LessonResponse(
                 lesson.getLessonId(),
                 lesson.getTitle(),
-                lesson.getTeacher()
+                lesson.getTeacher().getId(),
+                lesson.getTeacher().getName(),
+                studentIds,
+                studentNames
         );
     }
 }
