6b1957cc609013ecb5c2e2b5c8b0e038d88d0173
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-11-08 23:38:18 +0900

    fix: "test 코드 살림"

---
+++ src/test/java/oneclass/oneclass/OneclassApplicationTests.java
@@ -0,0 +1,13 @@
+package oneclass.oneclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
