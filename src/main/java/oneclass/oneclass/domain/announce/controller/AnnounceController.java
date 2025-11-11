cef6eeda0452ccca7bf651dcc184d9809c5c57de
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-11-11 16:54:35 +0900

    fix: "announce만 swagger 문서정리 안되어 있던 거 고침"

--- src/main/java/oneclass/oneclass/domain/announce/controller/AnnounceController.java
+++ src/main/java/oneclass/oneclass/domain/announce/controller/AnnounceController.java
@@ -1,5 +1,6 @@
 package oneclass.oneclass.domain.announce.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import oneclass.oneclass.domain.announce.dto.request.CreateAnnounceRequest;
 import oneclass.oneclass.domain.announce.dto.request.UpdateAnnounceRequest;
@@ -16,31 +17,43 @@ import java.util.List;
 public class AnnounceController {
     private final AnnounceService announceService;
 
+    @Operation(summary = "공지 생성",
+            description = "공지를 만듭니다.")
     @PostMapping("/create")
     public AnnounceResponse createAnnounce(@RequestBody CreateAnnounceRequest request) {
         return announceService.createAnnounce(request);
     }
 
+    @Operation(summary = "검색",
+            description = "강의를 id로 찾습니다")
     @GetMapping("/id/{id}")
     public AnnounceResponse findAnnounceById(@PathVariable Long id) {
         return announceService.findAnnounceById(id);
     }
 
+    @Operation(summary = "검색",
+            description = "강의를 제목로 찾습니다")
     @GetMapping("/title/{title}")
     public AnnounceResponse findAnnounceByTitle(@PathVariable String title) {
         return announceService.findAnnounceByTitle(title);
     }
 
+    @Operation(summary = "강의 수정",
+            description = "강의를 수정합니다")
     @PatchMapping()
     public AnnounceResponse updateAnnounce(@RequestBody UpdateAnnounceRequest request) {
         return announceService.updateAnnounce(request);
     }
 
+    @Operation(summary = "강의 삭제",
+            description = "강의를 삭제합니다")
     @DeleteMapping("/{id}")
     public void deleteAnnounce(@PathVariable Long id) {
         announceService.deleteAnnounce(id);
     }
 
+    @Operation(summary = "검색",
+            description = "강의를 모두 찾습니다")
     @GetMapping("/all")
     public List<AnnounceResponse> findAll() {
         return announceService.findAll();
