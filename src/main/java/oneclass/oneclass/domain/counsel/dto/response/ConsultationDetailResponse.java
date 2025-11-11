6c2e562b683919e2cf257eb872814df581ef8280
김준현 <smileehd03@dgsw.hs.kr>
2025-11-11 20:44:29 +0900

    fix: 에러 고침 무튼 고침 ㅅㄱ

--- src/main/java/oneclass/oneclass/domain/counsel/dto/response/ConsultationDetailResponse.java
+++ src/main/java/oneclass/oneclass/domain/counsel/dto/response/ConsultationDetailResponse.java
@@ -1,13 +1,16 @@
 package oneclass.oneclass.domain.counsel.dto.response;
 
 import oneclass.oneclass.domain.counsel.entity.Consultation;
+import oneclass.oneclass.domain.counsel.entity.Gender;
 
 public record ConsultationDetailResponse(
         String name,
         String phone,
         String type,
         String subject,
-        String description
+        String description,
+        int age,
+        Gender gender
 ) {
     public static ConsultationDetailResponse from(Consultation consultation) {
         return new ConsultationDetailResponse(
@@ -15,7 +18,9 @@ public record ConsultationDetailResponse(
                 consultation.getPhone(),
                 consultation.getType(),
                 consultation.getSubject(),
-                consultation.getDescription()
+                consultation.getDescription(),
+                consultation.getAge(),
+                consultation.getGender()
         );
     }
 }
