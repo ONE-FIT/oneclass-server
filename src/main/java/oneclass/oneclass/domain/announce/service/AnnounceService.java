9fa88dac841dd03723f1d29362b8338977e31ced
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-11-18 16:43:48 +0900

    fix: 제미나이 의견 수용

--- src/main/java/oneclass/oneclass/domain/announce/service/AnnounceService.java
+++ src/main/java/oneclass/oneclass/domain/announce/service/AnnounceService.java
@@ -124,7 +124,7 @@ public class AnnounceService {
         }
 
 
-        return AnnounceResponse.of(announceRepository.save(announce));
+        return AnnounceResponse.of(announce);
     }
 
     @Transactional
